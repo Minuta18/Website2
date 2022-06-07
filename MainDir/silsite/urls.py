5b4ba7a287a5df83bef6a1c1541e77890e28dff0
Александр Володин <alexandervolodin2020@gmail.com>
2022-06-07 10:46:10 +0300

    Пока убрал css, создал ссылки: projects и project

--- MainDir/silsite/urls.py
+++ MainDir/silsite/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
   path('', views.base, name='base'),
   path('login/', views.login, name='login'),
+  path('projects/', views.projects_view, name='projects'),
+  path('<name: str>/', views.project_view, name='project'),
   # path('logout', views.logout, name='logout'),
 ]
--- MainDir/silsite/views.py
+++ MainDir/silsite/views.py
@@ -1,36 +1,44 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate
 from django.contrib import auth
-from django.contrib.auth import get_user
 from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
 
 
 @login_required(redirect_field_name='')
 def base(request):
-  
   return render(request, 'silsite/base.html')
 
+
 def login(request):
-  '''Страница входа'''
-  text = ''
-  form = LoginForm(data=(request.POST or None))
-  if request.method == 'POST':
-    if form.is_valid():
-      username = form.cleaned_data['username']
-      password = form.cleaned_data['password']
-      user = authenticate(username=username, password=password)
-      if user is not None:
-        if user.is_active: # TODO: Зачем? #Нужно. # Ок
-          auth.login(request, user)
-          return redirect('/')
+    """Страница входа"""
+    text = ''
+    form = LoginForm(data=(request.POST or None))
+    if request.method == 'POST':
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    auth.login(request, user)
+                    return redirect('/')
+                else:
+                    form = LoginForm()
+                    text = 'Sorry, but your account is inactive.'
+            else:
+                form = LoginForm()
+                text = 'Invalid username or password.'
         else:
           form = LoginForm()
-          text = 'Sorry, but your account is inactive.'
-      else:
-        form = LoginForm()
-        text = 'Invalid username or password.'
-    else:
-      form = LoginForm()
-      text = 'Please, fill the form.'
-  return render(request, 'silsite/login.html', {'form': form, 'text': text})
+          text = 'Please, fill the form.'
+    return render(request, 'silsite/login.html', {'form': form, 'text': text})
+
+
+def projects_view(request):
+    projects = None
+    return render(request, 'silsite/projects.html', {'projects': projects})
+
+
+def project_view(request, name):
+    return render(request, 'silsite/project.html', {'name': name})
