ffca53677889b68131b84b66adb5e8ea19931e7e
TimKostenok <95478896+TimKostenok@users.noreply.github.com>
2022-06-08 13:22:55 +0400

    Комит закомитен

--- MainDir/silsite/urls.py
+++ MainDir/silsite/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-  path('', views.base2, name='base'),
+  path('', views.main_page, name='main_page'),
   path('login', views.login, name='login'),
+  path('projects/project/<str:name>/', views.project_view, name='project_view'),
+  path('projects/new/', views.new_project, name='new_project'),
   path('logout', views.logout, name='logout'),
-]
+  path('error_404', views.error_404, name='error_404'),
+  path('projects/', views.projects_view, name='projects'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- MainDir/silsite/views.py
+++ MainDir/silsite/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate
 from django.contrib import auth
+from django.contrib.auth import get_user
 from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
-
+from .forms import ProjectForm
+from django.http import Http404
+from .models import Project
+from base64 import b64decode, b64encode
 
 @login_required(redirect_field_name='')
-def base2(request):
-  return render(request, 'silsite/base2.html')
+def main_page(request):
+  return render(request, 'silsite/main_page.html')
 
 def login(request):
   '''Страница входа'''
-  text = 'Sorry, but you have to log in before using site'
+  text = 'Sorry, but you have to login before using site.'
   form = LoginForm(data=(request.POST or None))
   if request.method == 'POST':
     if form.is_valid():
@@ -21,7 +25,7 @@ def login(request):
       if user is not None:
         if user.is_active:
           auth.login(request, user)
-          return redirect('base')
+          return redirect('/')
         else:
           form = LoginForm()
           text = 'Sorry, but your account is inactive.'
@@ -31,13 +35,41 @@ def login(request):
     else:
       form = LoginForm()
       text = 'Invalid username or password.'
-  else:
-    form = LoginForm()
-    text = 'Please, fill the form'
   return render(request, 'silsite/login.html', {'form': form, 'text': text})
 
+def new_project(request):
+  '''Новый проект'''
+  form = ProjectForm(request.POST or None)
+  if request.method == 'POST':
+    if form.is_valid():
+      prj = Project()
+      prj.name = form.cleaned_data['name']
+      prj.short_text = form.cleaned_data['short_text']
+      prj.text = form.cleaned_data['text']
+      prj.presentation = request.FILES['presentation']
+      prj.teacher = request.user
+      prj.save()
+      return redirect('projects')
+    else:
+      form = ProjectForm()
+  return render(request, 'silsite/new_project.html', {'form': form})
+
+def projects_view(request):
+    projects = Project.objects.all()
+    return render(request, 'silsite/projects.html', {'projects': projects})
+
+def project_view(request, name):
+  '''Просмотр проекта'''
+  try:
+    project = Project.objects.filter(name = name)[0]
+  except Project.DoesNotExist:
+    return render(request, 'silsite/error_404.html')
+  return render(request, 'silsite/project_view.html', {'project': project})
+
+@login_required(redirect_field_name='')
 def logout(request):
-  if (request.method == 'POST'):
+  '''Выход'''
+  if (request.method == 'POST'): 
     val = ''
     try:
       val = request.POST.get('Yes')
@@ -47,6 +79,9 @@ def logout(request):
       auth.logout(request)
       return redirect('login')
     else:
-      return redirect('base')
+      return redirect('main_page')
   else:
     return render(request, 'silsite/logout.html', {'username': request.user.username})
+
+def error_404(request):
+  return render(request, 'silsite/error_404')
