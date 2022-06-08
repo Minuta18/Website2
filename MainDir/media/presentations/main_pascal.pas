d55df33c11102fec936a5373af3062df9e677606
TimKostenok <95478896+TimKostenok@users.noreply.github.com>
2022-06-08 13:12:29 +0400

    Вроде все изменения собраны (кроме редактирования)

---
+++ MainDir/media/presentations/main_pascal.pas
@@ -0,0 +1,23 @@
+﻿begin
+  var s1 := ReadString;
+  var s2 := ReadString;
+  var (x, y) := (s1.ToBigInteger(), s2.ToBigInteger());
+  var z := x + 1;
+  while (z <= y) do
+  begin
+    x *= z;
+    z := z + 1;
+  end;
+  //Write(x);
+  while (x div 10 > 0) do
+  begin
+    y := 0;
+    while (x > 0) do
+    begin
+      y += x mod 10;
+      x := x div 10;
+    end;
+    x := y;
+  end;
+  Write(x);
+end.
