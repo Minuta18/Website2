b95372b6119ee7d066a2a370d1d717b133462fd4
Alexander-Volodi <80424611+Alexander-Volodi@users.noreply.github.com>
2022-06-15 13:29:00 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Project(models.Model):
+  '''Моделька проекта'''
+  name = models.CharField(max_length=31) # Название
+  text = models.FileField(upload_to='texts') # Описание
+  short_text = models.FileField(upload_to='short_texts') # Краткое описание
+  presentation = models.FileField(upload_to='presentations') # Преза
+  teacher = models.ForeignKey(User, on_delete=models.CASCADE) # Руководитель
+
+class Video(models.Model):
+  '''Видео'''
+  video_wishes = models.CharField(max_length=127) # Пожелания для видео
+  video = models.TextField() # Видео
+  project = models.ForeignKey(Project, on_delete=models.CASCADE, primary_key=True) # Проект
+  
